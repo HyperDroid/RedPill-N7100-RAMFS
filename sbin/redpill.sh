620e547eaa0729aeaf54476c42b7da84c8606b2e
Sar Castillo <sar.castillo@gmail.com>
2012-11-09 01:19:44 +0800

    Updated redpillinit script
    Signed-off-by: Sar Castillo <sar.castillo@gmail.com>

--- sbin/redpill.sh
+++ sbin/redpill.sh
@@ -11,8 +11,18 @@ echo "NO_GENTLE_FAIR_SLEEPERS" > /sys/kernel/debug/sched_features
 mount -o noatime,remount,ro,discard,barrier=0,commit=1,noauto_da_alloc,delalloc /system /system;
 mount -o noatime,remount,rw,discard,barrier=0,commit=1,noauto_da_alloc,delalloc /data /data;
 
-# FileSync Control (Disabled for better IO throughput)
+# FileSync Control (Disabled for better IO latency)
 echo 0 > /sys/class/misc/fsynccontrol/fsync_enabled
+
+# Camera Fix for Certain N7100 Devices
+if [ ! -f /data/cfw/SlimISP_*.bin ];
+then
+  cp /vendor/firmware/SlimISP_GK.bin /data/cfw/SlimISP_GK.bin
+  cp /vendor/firmware/SlimISP_ZK.bin /data/cfw/SlimISP_ZK.bin
+  chown system /data/cfw/*
+  chgrp media /data/cfw/*
+  chmod 0775 /data/cfw/*
+fi
 
 # SetCPU Min and Max Freq
 echo "1600000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
@@ -111,7 +121,7 @@ if [ -e /proc/sys/vm/swappiness ]; then
         echo "0" > /proc/sys/vm/swappiness
 fi
 if [ -e /proc/sys/vm/min_free_kbytes ]; then
-        echo "1536" > /proc/sys/vm/min_free_kbytes
+        echo "1536" > /proc/sys/vm/min_free_kbytes
 fi
 
 # Miscellaneous Net tweaks
@@ -175,20 +185,28 @@ if [ -e /proc/sys/fs/lease-break-time ]; then
 fi
 
 # Tweak cfq io scheduler
-  for i in $(/sbin/busybox ls -1 /sys/block/mmc*)
-  do echo "0" > $i/queue/rotational
-    echo "0" > $i/queue/iostats
-    echo "1" > $i/queue/iosched/group_isolation
-    echo "8" > $i/queue/iosched/quantum
-    echo "1" > $i/queue/iosched/low_latency
-    echo "5" > $i/queue/iosched/slice_idle
-    echo "2" > $i/queue/iosched/back_seek_penalty
-    echo "1000000000" > $i/queue/iosched/back_seek_max
-  done
+#  for i in $(/sbin/busybox ls -1 /sys/block/mmc*)
+#  do echo "0" > $i/queue/rotational
+#    echo "0" > $i/queue/iostats
+#   echo "1" > $i/queue/iosched/group_isolation
+#    echo "8" > $i/queue/iosched/quantum
+#    echo "1" > $i/queue/iosched/low_latency
+#    echo "5" > $i/queue/iosched/slice_idle
+#    echo "2" > $i/queue/iosched/back_seek_penalty
+#    echo "1000000000" > $i/queue/iosched/back_seek_max
+#  done
 
 # Power savings
 echo "2" > /sys/devices/system/cpu/sched_mc_power_savings
 echo "3" > /sys/module/cpuidle_exynos4/parameters/enable_mask
+
+# Boost SD Cards
+chmod 777 /sys/block/mmcblk0/queue/read_ahead_kb
+echo "4096" > /sys/block/mmcblk0/queue/read_ahead_kb
+chmod 777 /sys/block/mmcblk1/queue/read_ahead_kb
+echo "4096" > /sys/block/mmcblk1/queue/read_ahead_kb
+chmod 777 /sys/devices/virtual/bdi/179:0/read_ahead_kb
+echo "4096" > /sys/devices/virtual/bdi/179:0/read_ahead_kb
 
 # Run Init Scripts
 
@@ -198,21 +216,21 @@ fi
 
 # Backup EFS
 
-if [ ! -f /data/.redpill/efsbackup.tar.gz ];
-then
-  mkdir /data/.redpill
-  chmod 777 /data/.redpill
-  /sbin/busybox tar zcvf /data/.redpill/efsbackup.tar.gz /efs
-  /sbin/busybox cat /dev/block/mmcblk0p3 > /data/.redpill/efsdev-mmcblk0p3.img
-  /sbin/busybox gzip /data/.redpill/efsdev-mmcblk0p3.img
-  chmod 777 /data/.redpill/efsdev-mmcblk0p3.img
-  chmod 777 /data/.redpill/efsbackup.tar.gz
+#if [ ! -f /data/.redpill/efsbackup.tar.gz ];
+#then
+#  mkdir /data/.redpill
+#  chmod 777 /data/.redpill
+#  /sbin/busybox tar zcvf /data/.redpill/efsbackup.tar.gz /efs
+#  /sbin/busybox cat /dev/block/mmcblk0p3 > /data/.redpill/efsdev-mmcblk0p3.img
+#  /sbin/busybox gzip /data/.redpill/efsdev-mmcblk0p3.img
+#  chmod 777 /data/.redpill/efsdev-mmcblk0p3.img
+#  chmod 777 /data/.redpill/efsbackup.tar.gz
   #make sure that sdcard is mounted, media scanned..etc
-  (
-    sleep 1000
-    /sbin/busybox cp /data/.redpill/efs* /storage/sdcard0
-  ) &
-fi
+  #(
+  #  sleep 1000
+  #  /sbin/busybox cp /data/.redpill/efs* /storage/sdcard0
+  #) &
+#fi
 
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
 mount -o remount,ro /system
