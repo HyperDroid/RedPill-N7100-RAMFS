35e779905b5f4ce89bd1507de50983ed05ef4dc4
sarcastillo <sar.castillo@gmail.com>
2013-01-02 19:25:55 +0800

    STweaks:Updated to use Dynamic XML generation by AndreiLux
    Signed-off-by: sarcastillo <sar.castillo@gmail.com>

---
+++ res/customconfig/init.profile
@@ -0,0 +1,7 @@
+internal_iosched=sio
+sd_iosched=sio
+fsync_enabled=1
+logger=on
+led_fade=1
+ext2intexfat=off
+ext2intfat=off
--- res/uci.sh
+++ res/uci.sh
@@ -5,6 +5,7 @@
 # You probably won't need to modify this file
 # You'll need to modify the files in /res/customconfig directory
 
+ACTION_SCRIPTS=/res/customconfig/actions
 source /res/customconfig/customconfig-helper
 
 # first, read defaults
@@ -14,24 +15,24 @@ read_defaults
 read_config
 
 case "${1}" in
-#  rename)
-#    rename_profile "$2" "$3"
-#    exit 0
-#    ;;
+  rename)
+    rename_profile "$2" "$3"
+    exit 0
+    ;;
   delete)
     delete_profile "$2"
     exit 0
     ;;
-#  select)
-#    select_profile "$2"
-#    exit 0
-#    ;;
+  select)
+    select_profile "$2"
+    exit 0
+    ;;
   config)
     print_config
     ;;
-#  list)
-#    list_profile
-#    ;;
+  list)
+    list_profile
+    ;;
   apply)
     apply_config
     ;;
